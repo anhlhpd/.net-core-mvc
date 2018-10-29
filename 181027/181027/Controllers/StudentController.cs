ef41ecb74713ab2d2d74f4e795489b7e17092243
Phương Anh <anhlhpd00579@fpt.edu.vn>
2018-10-29 12:18:28 +0700

    1st commit

---
+++ 181027/181027/Controllers/StudentController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using _181027.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace _181027.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly StudentContext _context;
+        public StudentController(StudentContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(_context.Students.ToList());
+        }        
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Store(Student student)
+        {
+            _context.Students.Add(student);
+            _context.SaveChanges();
+            //return new JsonResult(student);
+            return RedirectToAction("Index");
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(long id, Student student)
+        {
+            var obj = _context.Students.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            obj.RollNumber = student.RollNumber;
+            obj.Name = student.Name;
+
+            _context.Students.Update(obj);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+            Debug.WriteLine(id);
+            var obj = _context.Students.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            _context.Students.Remove(obj);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ 181027/181027/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _181027.Models
+{
+    public class Student
+    {
+        public long Id { get; set; }
+        public string RollNumber { get; set; }
+        public string Name { get; set; }
+    }
+}
