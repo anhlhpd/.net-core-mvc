1eb8d0ed49d15bcad86ea584eef30ce0e349ab67
Phương Anh <anhlhpd00579@fpt.edu.vn>
2018-11-08 00:50:12 +0700

    3rd lesson

---
+++ 181106/181106/Data/_181106Context.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace _181106.Models
+{
+    public class _181106Context : DbContext
+    {
+        public _181106Context (DbContextOptions<_181106Context> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>().HasData(
+                new Student()
+                {
+                    RollNumber = "A0001",
+                    FirstName = "Hung",
+                    LastName = "Dao",
+                    Email = "daohung@gmail.com"
+                },
+                new Student()
+                {
+                    RollNumber = "A0002",
+                    FirstName = "Luyen",
+                    LastName = "Dao",
+                    Email = "daoluyen@gmail.com"
+                },
+                new Student()
+                {
+                    RollNumber = "A0003",
+                    FirstName = "Hong",
+                    LastName = "Dao",
+                    Email = "daohong@gmail.com"
+                }
+            );
+            modelBuilder.Entity<Mark>().HasData(
+                new Mark()
+                {
+                    Id = 1,
+                    SubjectName = "Toan",
+                    SubjectMark = 30,
+                    StudentRollNumber = "A0001"
+                },
+                new Mark()
+                {
+                    Id = 2,
+                    SubjectName = "Van",
+                    SubjectMark = 40,
+                    StudentRollNumber = "A0002"
+                },
+                new Mark()
+                {
+                    Id = 3,
+                    SubjectName = "Hoa",
+                    SubjectMark = 50,
+                    StudentRollNumber = "A0003"
+                }
+            );
+        }
+
+        public DbSet<_181106.Models.Student> Student { get; set; }
+        public DbSet<_181106.Models.Mark> Mark { get; set; }
+    }
+}
---
+++ 181106/181106/Models/Student.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _181106.Models
+{
+    public class Student
+    {
+        [Key]
+        public string RollNumber { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public StudentStatus Status { get; set; }
+
+        public List<Mark> Marks { get; set; }
+
+        public Student()
+        {
+            this.CreatedAt = DateTime.Now;
+            this.UpdatedAt = DateTime.Now;
+            this.Status = StudentStatus.Activated;
+        }
+        
+    }
+    public enum StudentStatus
+    {
+        Activated = 1,
+        Deactivated = 0
+    }
+}
