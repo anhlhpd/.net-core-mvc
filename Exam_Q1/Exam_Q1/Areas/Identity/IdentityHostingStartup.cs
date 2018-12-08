3d8f198029dd3f77e5c3920b7021499d9e8b3947
Phương Anh <anhlhpd00579@fpt.edu.vn>
2018-12-08 19:35:17 +0700

    Exam Q1

---
+++ Exam_Q1/Exam_Q1/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Exam_Q1.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Exam_Q1.Areas.Identity.IdentityHostingStartup))]
+namespace Exam_Q1.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<UserContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("UserContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>()
+                    .AddEntityFrameworkStores<UserContext>();
+            });
+        }
+    }
+}
